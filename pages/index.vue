299296d68155a021ae115088fed91a4c7da5195b
OleMan <aom-95@live.com>
2018-09-26 10:53:08 +0300

    EWS-0: main page - section-ews27

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
     <section-ews16/>
     <section-ews21/>
     <section-ews24/>
+    <section-ews27/>
   </section>
 </template>
 
@@ -12,6 +13,7 @@ import sectionEws3 from '~/components/sections/section-EWS-3/index.vue'
 import sectionEws16 from '~/components/sections/section-EWS-16/index.vue'
 import sectionEws21 from '~/components/sections/section-EWS-21/index.vue'
 import sectionEws24 from '~/components/sections/section-EWS-24/index.vue'
+import sectionEws27 from '~/components/sections/section-EWS-27/index.vue'
 
 export default {
   head: {
@@ -22,6 +24,7 @@ export default {
     sectionEws16,
     sectionEws21,
     sectionEws24,
+    sectionEws27,
   }
 
 }

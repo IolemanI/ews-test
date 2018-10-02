374257e48a11f41c355dc441feb50b4dfae16fc5
OleMan <aom-95@live.com>
2018-10-02 12:43:05 +0300

    EWS-0: fix controller

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,16 +1,11 @@
 // Mock Users
-const index = [
-  { name: 'Alexandre' },
-  { name: 'Pooya' },
-  { name: 'Sébastien' }
-]
+const count = 0
+
 
 let getCaseStudies = function (req, res, next) {
-
-
 
   res.set('Set-Cookie', [`count=${count}`]);
   res.send('okcxvxcv');
 }
 
-module.exports = {getClientInfo};
+module.exports = {getCaseStudies};

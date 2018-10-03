fdcabd783c2231bced38640cd93558d5ea7b1938
OleMan <aom-95@live.com>
2018-10-03 21:14:50 +0300

    EWS-0: section-ews24 mob styling

--- components/sections/section-EWS-24/index.vue
+++ components/sections/section-EWS-24/index.vue
@@ -80,24 +80,6 @@
         </div>
       </div>
 
-      <div class="case-studies hidden-md-up">
-        <div class="container">
-
-          <div class="content-mobile">
-            <div id="cases-carousel">
-
-              {{caseStudies}}
-
-            </div>
-
-            <div class="btn-view">
-              <a class="btn btn-rounded btn-outline-primary" href="/portfolio">{{view}}</a>
-            </div>
-          </div>
-
-        </div>
-      </div>
-
     </div>
   </section>
 </template>
@@ -118,25 +100,29 @@
           "High-Load Systems"
         ],
         view: "View all",
-        itemsPerSlide: 4,
         slide: 0,
-        sliding: null
+        sliding: null,
+        caseStudies: ''
       }
     },
     computed: {
-      caseStudies () {
-        let caseStudies = this.$store.state.casestudies.list;
-        let tempCaseStudies = [];
-
-        for (let i = 0; i < caseStudies.length; i += this.itemsPerSlide) {
-          tempCaseStudies.push(caseStudies.slice(i, i + this.itemsPerSlide));
-        }
-
-        return tempCaseStudies
-      }
     },
     methods: {
+    },
+    mounted(){
+      let caseStudies = this.$store.state.casestudies.list;
+      let tempCaseStudies = [];
+      let itemsPerSlide = 4;
 
+      if(window.screen.availWidth <= 480){
+        itemsPerSlide = 3;
+      }
+
+      for (let i = 0; i < caseStudies.length; i += itemsPerSlide) {
+        tempCaseStudies.push(caseStudies.slice(i, i + itemsPerSlide))
+      }
+
+      this.caseStudies = tempCaseStudies
     }
   }
 </script>
@@ -159,7 +145,7 @@
   $carousel-active-navigation: #20ABE2;
   $content-margin: 0 1.5rem;
   $section-subtitle-color: #848484;
-  $margin-view-all: 1.6rem auto 0;
+  $margin-view-all: 1.6rem auto 1rem;
 
   .section-ews24 {
     padding-bottom: 2.5rem;
@@ -254,12 +240,17 @@
     .wrapper {
       margin-left: 0.1rem;
       margin-right: 0.1rem;
+      padding-top: 4.5rem;
     }
 
     .filter{
       margin-left: 3rem;
       margin-right: 3rem;
       padding-right: 10px;
+
+      @media(max-width: $sm) {
+        display: none;
+      }
     }
 
     .cards {
@@ -272,8 +263,16 @@
       }
 
       @media(max-width: $sm) {
-        margin-left: 1rem;
-        margin-right: 1rem;
+        margin-left: .5rem;
+        margin-right: .5rem;
+        margin-bottom: 7rem;
+
+        .case-study-slide{
+
+          .case-study{
+            padding: 0;
+          }
+        }
       }
     }
 
@@ -296,10 +295,6 @@
         cursor: pointer;
         height: 20.2rem;
         overflow: hidden;
-
-        .card-img-top {
-          border-radius: 0;
-        }
 
         .hidden-desc {
           display: none;
@@ -365,6 +360,19 @@
           text-align: center;
           display: flex;
           justify-content: center;
+
+          .card-img-top {
+            border-radius: 0;
+          }
+          @media(max-width: $sm) {
+            height: 9.5rem;
+            padding-top: 2.8rem;
+
+            .card-img-top {
+              height: 6.8rem;
+              width: unset;
+            }
+          }
         }
 
         .img-107-windscribe{
@@ -437,266 +445,16 @@
             }
           }
         }
-      }
-    }
-  }
-
-  .content-mobile {
-    height: auto;
-    background-color: $white;
-    -webkit-box-shadow: -2px 0.56rem 2.18rem -4px rgba(0, 0, 0, 0.2);
-    -moz-box-shadow: -2px 0.56rem 2.18rem -4px rgba(0, 0, 0, 0.2);
-    box-shadow: -2px 0.56rem 2.18rem -4px rgba(0, 0, 0, 0.2);
-    padding: 0 0.5rem 1.5rem;
-
-    .subtitle {
-      text-align: center;
-      padding-bottom: 1.3rem;
-      color: $section-subtitle-color;
-    }
-
-    .card {
-      border-radius: 0;
-      border: 1px solid $carousel-post-border-color;
-      margin-top: 0.7rem;
-      cursor: pointer;
-      height: 20.2rem;
-      overflow: hidden;
-
-      &:hover {
-        border: none;
-      }
-
-      a:focus {
-        text-decoration: none;
-        outline: none;
-      }
-
-      .card-img-top {
-        border-radius: 0;
-      }
-
-      .hidden-desc {
-        display: none;
-      }
-
-      .card-block {
-        padding: 0.5rem 1.4rem;
-        height: 6.3rem;
-
-        a {
-          text-decoration: none;
-        }
 
         @media(max-width: $sm) {
-          padding: 0.2rem 1.4rem;
+          margin: 0 0 0.5rem 0;
+          height: unset;
         }
       }
-
-      .card-tags {
-        list-style: none;
-        text-transform: uppercase;
-        color: $carousel-card-tags-color;
-        font-size: 0.875rem;
-        margin-bottom: 1rem;
-        padding: 0;
-        width: 100%;
-        max-height: 1.5rem;
-        overflow: hidden;
-        white-space: normal;
-
-        li {
-          display: inline-flex;
-          padding-right: 1.25rem;
-          line-height: 2;
-
-          &:last-child {
-            padding-right: 0;
-          }
-        }
-
-        @media(max-width: $sm) {
-          color: #8598A7;
-          margin-top: 0.8rem;
-        }
-      }
-
-      .card-title {
-        color: $color-primary;
-        text-transform: uppercase;
-        font-weight: 700;
-        font-size: 1.125rem;
-        width: 14rem;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        margin-top: -0.43rem;
-      }
-
-      .arrow {
-        position: absolute;
-        margin-top: -3.3rem;
-        right: 1.8rem;
-        font-size: 1.5rem;
-        color: $carousel-card-arrow-color;
-
-        @media(max-width: $sm) {
-          margin-top: -2.8rem;
-          right: 1rem;
-        }
-      }
-
-      .card-img-bg {
-        height: 13.8rem;
-        padding-top: 2.8rem;
-
-        @media(max-width: $sm) {
-          height: 9.5rem;
-        }
-      }
-
-      .image {
-        margin-left: auto;
-        margin-right: auto;
-        height: 10rem;
-        background-size: cover;
-        background-repeat: no-repeat;
-
-        @media(max-width: $sm) {
-          height: 6.8rem;
-        }
-      }
-
-      &:hover {
-        transition: all $default-animation-speed;
-        border: none;
-        -webkit-box-shadow: 1px 2px 2.5rem 2px rgba(218, 221, 221, 0.3);
-        -moz-box-shadow: 1px 2px 2.5rem 2px rgba(218, 221, 221, 0.3);
-        box-shadow: 0 2px 30px rgba(0, 0, 0, 0.1);
-
-        .arrow {
-          color: $color-secondary;
-        }
-
-        .hidden-desc {
-          display: block;
-          font-size: 0.875rem;
-          text-align: left;
-          padding-right: 1rem;
-          position: absolute;
-          animation: show-desc-hover;
-          color: $color-primary;
-          animation-duration: 0.2s;
-          background-color: $white;
-          overflow: hidden;
-          height: 4.3rem;
-          text-overflow: ellipsis;
-          transition: all .2s linear;
-          -o-transition: all .2s linear;
-          -moz-transition: all .2s linear;
-          -webkit-transition: all .2s linear;
-          text-overflow: ellipsis;
-          display: -webkit-box;
-          -webkit-box-orient: vertical;
-          -webkit-line-clamp: 3;
-        }
-
-        .card-block {
-          background: $white;
-          height: 11.5rem;
-          position: absolute;
-          top: 14.5rem;
-          width: 100%;
-          animation: show-desc-hover;
-          animation-duration: 0.2s;
-          transition: all .2s linear;
-          margin-top: -5.93rem;
-        }
-
-        @media(max-width: $sm) {
-          & {
-            height: 16rem;
-          }
-        }
-      }
-
-      @media(max-width: $sm) {
-        height: 15rem;
-        margin-bottom: 0.2rem;
-      }
-    }
-
-    .btn-outline-primary:focus {
-      box-shadow: none;
     }
 
     @media(max-width: $sm) {
-      .card:hover.mobile-hover {
-        .card-block {
-          background: $color-primary;
-          height: 16rem;
-          top: 0;
-          margin-top: 0;
-          color: $white;
-        }
-
-        .card-title {
-          color: $white;
-          margin-top: 0.6rem;
-        }
-
-        .hidden-desc {
-          background-color: $color-primary;
-          color: $white;
-          font-weight: normal;
-          line-height: 1.5rem;
-        }
-
-        .arrow {
-          display: none;
-        }
-
-        .read-more {
-          display: block;
-          color: $white;
-          position: absolute;
-          bottom: 1rem;
-          right: 0.7rem;
-          border: 1px solid $white;
-          padding: 0.2rem 2.8rem 0.2rem 1.2rem;
-          border-radius: 0.2rem;
-          border-color: rgba(255, 255, 255, 0.5);
-
-          &:hover, &:active, &:focus {
-            color: $color-primary;
-            background-color: $white;
-            .circle {
-              background-color: $color-primary;
-              color: $white;
-            }
-          }
-
-          .circle {
-            position: absolute;
-            width: 1rem;
-            top: 0.5rem;
-            right: 1rem;
-            height: 1rem;
-            font-weight: bold;
-            text-align: center;
-            border-radius: 50%;
-            background-color: $white;
-            color: $color-primary;
-            padding-left: 2px;
-
-            .ion-ios-arrow-forward {
-              margin-top: -4px;
-              &:before {
-                font-weight: bold;
-              }
-            }
-          }
-        }
-      }
+      margin-bottom: 70px;
     }
   }
 
@@ -760,6 +518,8 @@
 </style>
 
 <style lang="scss">
+  @import "~/assets/styles/index.scss";
+
   #cs_carousel{
     height: 740px;
 
@@ -787,6 +547,16 @@
       &.active{
         background: #20abe2;
       }
+    }
+
+    @media(max-width: $sm) {
+      bottom: -5rem !important;
+    }
+  }
+
+  #cs_carousel___BV_inner_{
+    .carousel-caption{
+      padding: 0;
     }
   }
 </style>

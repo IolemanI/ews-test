28f1071bd9ff882506dcc7af0a679f79aaec7418
OleMan <aom-95@live.com>
2018-10-03 15:20:49 +0300

    EWS-0: mob header

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,6 @@
   <header>
     <b-navbar toggleable="md" type="dark" variant="info">
       <div class="container">
-        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
         <nuxt-link to="/" class="navbar-brand">Eliftech</nuxt-link>
 
@@ -12,9 +11,9 @@
             <b-nav-item nuxt to="/forstartups" class="forstartups-item">FOR STARTUPS</b-nav-item>
 
             <li class="my-dropdown nav-item b-nav-dropdown dropdown">
-              <a href="#" aria-haspopup="true" aria-expanded="true" class="nav-link dropdown-toggle">
+              <button aria-haspopup="true" aria-expanded="true" class="nav-link dropdown-toggle">
                 WHAT WE DO
-              </a>
+              </button>
               <div class="dropdown-menu dropdown-menu-left show">
                 <nuxt-link to="/services" class="dropdown-item">SERVICES</nuxt-link>
                 <nuxt-link to="/expertise" class="dropdown-item">EXPERTISE</nuxt-link>
@@ -22,9 +21,9 @@
               </div>
             </li>
             <li class="my-dropdown nav-item b-nav-dropdown dropdown">
-              <a href="#" aria-haspopup="true" aria-expanded="true" class="nav-link dropdown-toggle">
+              <button aria-haspopup="true" aria-expanded="true" class="nav-link dropdown-toggle">
                 ABOUT US
-              </a>
+              </button>
               <div class="dropdown-menu dropdown-menu-left show">
                 <nuxt-link to="/who-we-are" class="dropdown-item">WHO WE ARE</nuxt-link>
                 <nuxt-link to="/career" class="dropdown-item">CAREER</nuxt-link>
@@ -36,6 +35,8 @@
           </b-navbar-nav>
 
         </b-collapse>
+
+        <b-navbar-toggle target="nav_collapse" class="toggler-icon-d-none"></b-navbar-toggle>
 
       </div>
 
@@ -85,6 +86,10 @@
   @import "~/assets/styles/index.scss";
 
   $header-animation-speed: 500ms;
+  $navbarBrandMargin: 10px 0 0 25px;
+  $header-toggler-color: #FFF;
+  $header-toggler-color-dark: $color-primary;
+
   header{
     background-color: transparent !important;
     position: fixed;
@@ -95,17 +100,28 @@
 
     &.bg-white{
       background-color: white !important;
+
+      .navbar .navbar-toggler{
+        background: str-replace(url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='#{$header-toggler-color-dark}' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E"), "#", "%23") no-repeat center;
+        background-size: 30px;
+      }
     }
   }
 
   .container{
-    min-height: 90px;
+    min-height: 60px;
     max-width: 960px;
+    position: relative;
+
+    @media(max-width: $sm) {
+      min-height: 90px;
+      align-items: flex-start;
+    }
   }
 
   .navbar{
     padding: 0;
-    min-height: 90px;
+    min-height: 66px;
     border-bottom: 0 solid transparent;
     transition: background .5s;
     backface-visibility: hidden;
@@ -125,9 +141,26 @@
       &.logo-color{
         background: url('/images/header-logo-color.png') no-repeat center left;
       }
+
+      @media(max-width: $sm) {
+        margin: $navbarBrandMargin;
+        background-size: 138px 45px;
+        background-image: url('/images/header-logo@2x.png');
+
+        &.logo-color{
+          background-size: 138px 45px;
+          background-image: url('/images/header-logo-color@2x.png');
+        }
+      }
     }
 
     .collapse{
+
+      .navbar-nav{
+        @media(max-width: $sm) {
+          padding-top: 5rem;
+        }
+      }
 
       .nav-item{
         .nav-link{
@@ -146,6 +179,15 @@
           &:hover{
             border-top: 5px solid #13a6e1;
           }
+          @media(max-width: $sm) {
+            color: #0B314F;
+            padding: .8rem 2.5rem;
+            min-height: 0;
+            margin: 0;
+            font-size: .875rem;
+            font-weight: 700;
+            font-family: Roboto,sans-serif;
+          }
         }
         &.forstartups-item{
           .nav-link{
@@ -156,12 +198,23 @@
             line-height: 1.3;
             border: 0;
             margin-top: 7px;
+
+            @media(max-width: $sm) {
+              display: inline-block;
+              margin: .8rem 2.5rem;
+            }
           }
         }
 
       }
 
       .my-dropdown{
+        .dropdown-toggle{
+          background: none;
+          border: 0;
+          width: 100%;
+          text-align: left;
+        }
         .dropdown-item{
           min-height: unset;
           box-shadow: 0 0 .1px 0 #173c58;
@@ -185,6 +238,47 @@
       }
 
     }
+
+    .navbar-toggler{
+      position: absolute;
+      top: 22px;
+      right: 1rem;
+      border-color: transparent;
+      border-radius: 3px;
+      outline: none;
+      background: str-replace(url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='#{$header-toggler-color}' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E"), "#", "%23") no-repeat center;
+      background-size: 30px;
+      width: 30px;
+      height: 30px;
+      padding: 4px 12px;
+      box-sizing: initial;
+    }
+
+    .navbar-collapse{
+      @media(max-width: $sm) {
+        background-color: white;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        right: 0;
+        width: 300px;
+
+        &.show + .navbar-toggler{
+          background: none;
+          font-family: "Ionicons",sans-serif;
+          width: 30px;
+          height: 30px;
+          &:before{
+            content: "\f2c0";
+            font-size: 2.6rem;
+            line-height: 1;
+            padding: 4px 8px;
+            color: $color-primary;
+          }
+        }
+
+      }
+    }
   }
 
   a{
@@ -196,5 +290,10 @@
   .my-dropdown:hover>.dropdown-menu.show{
     display: block;
   }
+</style>
 
+<style lang="scss">
+  .toggler-icon-d-none span{
+    display: none;
+  }
 </style>
